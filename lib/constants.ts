6de6dd15846c721dfc7d3a55ff081a2ac8ed3c24
Benjamin Donnaloia <umeboshi@Benjamins-Air.lan>
2025-01-12 23:57:31 -0700

    fix spacing

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,7 +5,7 @@ export const API_BASE_URLS = {
     permissions: 'http://localhost:8000',
   } as const
   
-  export const API_ENDPOINTS = {
+export const API_ENDPOINTS = {
     login: `${API_BASE_URLS.auth}/login`,
     profiles: (orgId: string) => `${API_BASE_URLS.profiles}/api/v1/organizations/${orgId}/profiles`,
     permissions: (userId: string) => `${API_BASE_URLS.permissions}/api/permissions/${userId}`,
